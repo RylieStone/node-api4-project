b0347c1161dfcbba2b6f43c4ffa40a2abad915ba
Rylie <rylie.stone2005@gmail.com>
2024-04-19 15:35:26 -0400

    added database

---
+++ server/methods/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        id: 1,
+        username: 'nethaniel',
+        password: 'firewall'
+    },
+    {
+        id: 2,
+        username: 'roger',
+        password: 'couper'
+    },
+    {
+        id: 3,
+        username: 'lever',
+        password: 'fumbler'
+    }
+]
+module.exports = users
